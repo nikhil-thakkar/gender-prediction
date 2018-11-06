9c78aaae4228f7b0f7315e73803a71deffe37b42
nikhil-thakkar <nikhil.thakkar1987@gmail.com>
2018-11-06 20:14:51 +0530

    Initial commit

---
+++ run.sh
@@ -0,0 +1,14 @@
+
+
+echo 'setting up python environment and installing other dependencies!';
+
+echo 'installing python 3.5';
+pyenv install 3.5.6;
+pyenv local 3.5.6;
+pip install -r requirements.txt;
+
+echo 'env setup completed';
+echo '........';
+echo '.................';
+echo 'run -> python realtime_demo.py to check the demo';
+
